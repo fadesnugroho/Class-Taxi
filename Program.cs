02b7aecc09b4333b0382aeae4ab2cbd8e486b630
fadesnugroho <fadesnugroho@students.amikom.ac.id>
2022-03-24 15:32:29 +0800

    tugas kedua

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taxi
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // penentuan nilai properties
+            taxi.DriverName = "Fades";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
